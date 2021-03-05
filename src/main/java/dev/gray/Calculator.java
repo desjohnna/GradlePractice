be52ed865ec1e270cb1aa5e24407911916eb4184
DJ <desjohnna@gmail.com>
2021-03-05 13:52:18 -0600

    adding files

---
+++ src/main/java/dev/gray/Calculator.java
@@ -0,0 +1,9 @@
+package dev.gray;
+
+public class Calculator {
+
+    public int add(String input){
+        return -1;
+    }
+
+}
---
+++ src/test/java/dev/gray/CalculatorTest.java
@@ -0,0 +1,21 @@
+package dev.gray;
+
+
+/*
+        Create a string calculator with and add method which returns an int
+            -the calculator should take up to two number, separated by a comma
+            -an empty string should return 0
+            -an exception is thrown if invalid input
+ */
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testEmptyString(){
+
+    }
+
+
+}
